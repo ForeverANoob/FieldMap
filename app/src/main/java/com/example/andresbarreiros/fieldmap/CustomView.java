df1e4ec97d93ce12752591cc82517813b6e16843
Andre Barreiros <andresbarreiros2012@gmail.com>
2018-01-24 16:55:19 +0200

    #MakingLinesTHICCagain

--- app/src/main/java/com/example/andresbarreiros/fieldmap/CustomView.java
+++ app/src/main/java/com/example/andresbarreiros/fieldmap/CustomView.java
@@ -18,6 +18,11 @@ public class CustomView extends View {
     private Rect rect2;
     private Paint paint;
 
+    private Paint background;
+    private Paint barriers;
+    private Paint path;
+    private Paint user;
+
     public CustomView(Context context){
         super(context);
         int x = 100;
@@ -31,13 +36,28 @@ public class CustomView extends View {
 
         paint = new Paint();
         paint.setColor(Color.GRAY);
+
+        background = new Paint();
+        barriers = new Paint();
+        path = new Paint();
+        user = new Paint();
+
+        background.setColor(Color.parseColor("#3394cc")); // 0x3394cc
+        barriers.setColor(Color.parseColor("#333333")); // 0xaaaaaa
+        path.setColor(Color.parseColor("#990099"));
+        user.setColor(Color.parseColor("#990011"));
+
+        path.setStrokeWidth(10);        // making lines THICC agsin
+        path.setStyle(Paint.Style.STROKE);
     }
 
     @Override
     protected void onDraw(Canvas canvas){
-        canvas.drawColor(Color.BLUE);
-        canvas.drawRect(rectangle, paint);
-        canvas.drawRect(rect2, paint);
+        canvas.drawColor(Color.parseColor("#3394cc"));
+        canvas.drawRect(rectangle, barriers);
+        canvas.drawRect(rect2, barriers);
+        canvas.drawLine(350,350,350,800, path);
+        canvas.drawCircle(800,800,20,user);
     }
 
 }
